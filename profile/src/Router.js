2fcdbf85fce7e5f791a163d57a95f08adba3fecb
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-02-02 21:33:18 +0100

    Add links to different routes

--- profile/src/Router.js
+++ profile/src/Router.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile';
 
 const Routes = () => {
     return(
         <BrowserRouter>
+            <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/profile">Profile</Link>
+            </li>
+            </ul>
+
+            <hr />
             <Switch>
                 <Route exact path='/' component={App} />
                 <Route exact path='/profile' component={Profile} />
